41e48537401af7344e057ac9bb4ff66870e268af
pfannkuchen <github.pfannkuchen@gougoule.ch>
2024-03-21 20:11:47 +0100

    Adding assets visualisation (not perfect but wip)

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -9,6 +9,11 @@
   </div>
   <div class="assets">
     <p>Assets :</p>
+    <svg class="assets-svg" viewBox="0 0 100000 5000" height="5000">
+      <rect rx="2000" ry="2000" style="fill:#fc7fe8;fill-opacity:1" x="0" y="0" transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)" width="10000" height="5000"></rect>
+      <rect rx="2000" ry="2000" style="fill:#62cfff;fill-opacity:1" x="11000" y="0" transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)" width="50000" height="5000"></rect>
+      <rect rx="2000" ry="2000" style="fill:#ffaf6e;fill-opacity:1" x="62000" y="0" transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)" width="38000" height="5000"></rect>
+    </svg>
   </div>
   <div class="line-chart">
 
@@ -51,6 +56,7 @@
     box-shadow: 0 0 20px rgb(174, 174, 174);
     font-size: 1.4em;
     padding-left: 2%;
+    padding-right: 2%;
   }
   .line-chart {
     grid-row: 3 / 4;
@@ -58,10 +64,21 @@
     border-radius: 1.5rem;
     box-shadow: 0 0 20px rgb(174, 174, 174);
   }
+
   .history {
     grid-row: 1 / 4;
     grid-column: 2 / 3;
     border-radius: 1.5rem;
     box-shadow: 0 0 20px rgb(174, 174, 174);
+  }
+
+  .assets p {
+    justify-content: center;
+    margin-bottom: 0.3em;
+  }
+
+  .assets-svg {
+    max-width: 100%;
+    height: 1.8em;
   }
 </style>
