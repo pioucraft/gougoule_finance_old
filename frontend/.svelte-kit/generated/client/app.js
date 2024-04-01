2b7ae6bf4721fcf2bc222345c1ac32fa76bcc4e2
Pfannkuchen <github.pfannkuchen@gougoule.ch>
2024-04-01 14:24:34 +0200

    add an account page

--- frontend/.svelte-kit/generated/client/app.js
+++ frontend/.svelte-kit/generated/client/app.js
@@ -7,7 +7,8 @@ export const nodes = [
 	() => import('./nodes/3'),
 	() => import('./nodes/4'),
 	() => import('./nodes/5'),
-	() => import('./nodes/6')
+	() => import('./nodes/6'),
+	() => import('./nodes/7')
 ];
 
 export const server_loads = [];
@@ -15,6 +16,7 @@ export const server_loads = [];
 export const dictionary = {
 		"/": [2],
 		"/accounts": [4],
+		"/addAccount": [7],
 		"/addTransaction": [6],
 		"/settings": [5],
 		"/user": [3]
