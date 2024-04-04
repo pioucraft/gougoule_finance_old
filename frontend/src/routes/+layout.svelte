422a0a36ea4828c0c4173e6e91e21d12a126552e
Pfannkuchen <github.pfannkuchen@gougoule.ch>
2024-04-04 09:29:51 +0200

    see the username on the frontend

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -7,7 +7,7 @@
     </div>
     
 
-    <a href="/user" class="transparentButton leftBarButton" on:click={() => changePressedButton("user")} id="leftBar-userButton"><svg id="leftBar-userButton-svg" data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" stroke-linecap="round" stroke-linejoin="round"></path></svg> Username</a>
+    <a href="/user" class="transparentButton leftBarButton" on:click={() => changePressedButton("user")} id="leftBar-userButton"><svg id="leftBar-userButton-svg" data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" stroke-linecap="round" stroke-linejoin="round"></path></svg> {username}</a>
 </div>
 
 <script>
@@ -15,6 +15,8 @@
     import { onMount } from "svelte";
     import { goto } from '$app/navigation';
     import axios from "axios"
+
+    let username = "Username"
 
     function changePressedButton(id) {
         document.getElementById("leftBar-homeButton").style = "background-color: auto";
@@ -44,9 +46,7 @@
                 catch(err) {
                     goto("/login")
                 }
-                
-
-                console.log(loginFetch)
+                username = loginFetch.data.username
             }
             
         } 
