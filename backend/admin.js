5a1bff3154b5b56352fe2f6bc6f048012fa4dba1
Pfannkuchen <hello@gougoule.ch>
2024-04-23 13:31:27 +0200

    modify user

--- backend/admin.js
+++ backend/admin.js
@@ -8,19 +8,22 @@ let answer = await inquirer.prompt({
     "message": "What do you want to do ?", 
     choices: [
         "Create a new user", 
-        "Delete an existing user", 
-        "Manage admins"
+        "Modify an existing user", 
+        "Delete an existing user"
     ]
 })
 
-switch(answer.command) {
-    case "Create a new user": {
-        createUserWithPrompt()
-    }
+if(answer.command == "Create a new user") {
+    createUser();
+}
+else if(answer.command == "Modify an existing user") {
+    modifyUser();
+}
+else if(answer.command == "Delete an existing user") {
+    deleteUser()
 }
 
-async function createUserWithPrompt() {
-    console.log("\nCreating a new user...\n")
+async function createUser() {
     let userData = await inquirer.prompt([
         {
             "name": "name",  
@@ -43,4 +46,58 @@ async function createUserWithPrompt() {
     await DBClient.query("INSERT INTO users (name, email, password, storage) VALUES ($1, $2, $3, $4)", [userData.name, userData.email, userData.password, userData.storage])
 
     console.log("New user successfully created, you can now safely Ctrl+C this script. DON'T FORGET TO RUN 'bun migrations.js'")
+}
+
+async function modifyUser() {
+    let userData = await inquirer.prompt([
+        {
+            "name": "oldName",
+            "message": "Enter the current name of your user : "
+        },
+        {
+            "name": "name",  
+            "message": "Enter the new name (leave empty for unchanged) : ", 
+        },
+        {
+            "name": "email",  
+            "message": "Enter the new email (leave empty for unchanged) : ", 
+        },
+        {
+            "name": "password",  
+            "message": "Enter the new password (leave empty for unchanged) : ", 
+        },
+        {
+            "name": "storage",
+            "message": "Enter the new storage path (leave empty for unchanged) : "
+        }
+    ])
+
+    if(userData.name) {
+        await DBClient.query("UPDATE users SET name = $1 WHERE name = $2", [userData.name, userData.oldName]);
+    }
+    if(userData.email) {
+        await DBClient.query("UPDATE users SET email = $1 WHERE name = $2", [userData.email, userData.oldName]);
+    }
+    if(userData.storage) {
+        console.log("storage")
+        await DBClient.query("UPDATE users SET storage = $1 WHERE name = $2", [userData.storage, userData.oldName]);
+    }
+    if(userData.password) {
+        userData.password = await Bun.password.hash(sha256(userData.password));
+        await DBClient.query("UPDATE users SET password = $1 WHERE name = $2", [userData.password, userData.oldName]);
+    }
+    console.log("Success !")
+
+    DBClient.end()
+}
+
+async function deleteUser() {
+    let userData = await inquirer.prompt([
+        {
+            "name": "name",
+            "message": "Enter the name of the user you want to delete : "
+        }
+    ])
+
+    DBClient.end()
 }
