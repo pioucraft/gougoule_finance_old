9b58d6028badbf232c92e334804642baaf547947
Pfannkuchen <hello@gougoule.ch>
2024-04-29 19:13:45 +0200

    more refactoring

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -1,3 +1,19 @@
+<div id="login">
+    <div id="login-box">
+        <div id="login-box-input">
+            <input type="text" class="login-text" id="email" placeholder="Email">
+            <input type="password" class="login-text" id="password" placeholder="Password">
+        </div>
+        <div id="login-box-buttons">
+            <p id="login-box-text">By logging in, you accept the use of cookies (only to store credentials).</p>
+            <button on:click={login} class="login-button">Log in</button>
+        </div>
+        
+    </div>
+</div>
+
+<p id="imageBy">Image by  Richard Horvath</p>
+
 <script>
     import sha256 from "js-sha256"
     import { getCookie, setCookie, deleteCookie } from 'svelte-cookie';
@@ -16,19 +32,3 @@
 <style>
     @import "style.css";
 </style>
-
-<div id="login">
-    <div id="login-box">
-        <div id="login-box-input">
-            <input type="text" class="login-text" id="email" placeholder="Email">
-            <input type="password" class="login-text" id="password" placeholder="Password">
-        </div>
-        <div id="login-box-buttons">
-            <p id="login-box-text">By logging in, you accept the use of cookies (only to store credentials).</p>
-            <button on:click={login} class="login-button">Log in</button>
-        </div>
-        
-    </div>
-</div>
-
-<p id="imageBy">Image by  Richard Horvath</p>
