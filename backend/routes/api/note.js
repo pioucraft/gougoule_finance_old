fb45947eab2514719fcee861159dcfe4a5ae0b9b
Pfannkuchen <hello@gougoule.ch>
2024-05-02 07:03:42 +0200

    wip

--- backend/routes/api/note.js
+++ backend/routes/api/note.js
@@ -14,4 +14,32 @@ export async function note(req) {
     let userId = (await DBClient.query("SELECT * FROM users where email = $1", [body.email])).rows[0]["id"]
     
     createUserFolder(userId)
+
+    if(req.method == "POST") return createNote(body, userId)
+    else if(req.method == "PUT") return modifyNote(body, userId)
+    else if(req.method == "PATCH") return deleteNote(body, userId)
+}
+
+function createNote(body, userId) {
+    // body : {location, name (without directory)}
+    Bun.write(`${__dirname}/../../userFiles/${userId}/notes/${body.location}/${body.name}`, "")
+    return new Response("200 Success")
+}
+
+function modifyNote(body, userId) {
+    // body : {location (folder to modify), name (new name ), content (full content of file)}
+    if(body.name) {
+        let newLocation = body.location.split("/").slice(0,-1).join("/") + "/" + body.name;
+        fs.renameSync(`${__dirname}/../../userFiles/${userId}/notes/${body.location}`, `${__dirname}/../../userFiles/${userId}/notes/${newLocation}`)
+    }
+    else if(body.content) {
+        Bun.write(`${__dirname}/../../userFiles/${userId}/notes/${body.location}`, body.content)
+    }
+    return new Response("200 Success")
+}
+
+function deleteNote(body, userId) {
+    // body : {location (folder to delete)}
+    fs.rmSync(`${__dirname}/../../userFiles/${userId}/notes/${body.location}`,  { recursive: true, force: true })
+    return new Response("200 Success")
 }
