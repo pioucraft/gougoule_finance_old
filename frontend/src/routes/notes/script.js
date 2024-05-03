c16aa8158c46dbeafd032cb66e3ebd36ee80c786
Pfannkuchen <hello@gougoule.ch>
2024-05-03 07:10:40 +0200

    wip

--- frontend/src/routes/notes/script.js
+++ frontend/src/routes/notes/script.js
@@ -2,10 +2,10 @@ import { getCookie } from 'svelte-cookie';
 import axios from "axios"
 
 export function expandFolder(location, filesAndFolders) {
-    if(!location) return [filesAndFolders.filter(x => !x.includes("/")), ""];
+    if(!location) return [filesAndFolders.filter(x => !x.includes("/")), "", ""];
     
     filesAndFolders = filesAndFolders.filter(x => x.startsWith(`${location}/`) && !x.split(`${location}/`)[1].includes("/"))
-    return [filesAndFolders, location]
+    return [filesAndFolders, location, location]
 }
 
 export async function makeData(url) {
@@ -47,6 +47,22 @@ export async function createNewNote(url, location) {
     }
     let filesAndFolders = await makeData(url)
     let currentFilesAndFolders = expandFolder(location, filesAndFolders)[0]
-    console.log(filesAndFolders)
     return [filesAndFolders, currentFilesAndFolders]
+}
+
+export async function editNote(url, location) {
+    try {
+        let newName = prompt("Enter the new name")
+        let password = getCookie("password")
+        let email = getCookie("email")
+        
+        await axios.put(`${url}/api/note`, JSON.stringify({"email": email, "password": password, "name": newName, "location": location}))
+    }
+    catch(err) {
+        alert("Error")
+    }
+    let filesAndFolders = await makeData(url)
+    let currentFilesAndFolders = expandFolder("", filesAndFolders)[0]
+    console.log(currentFilesAndFolders)
+    return [filesAndFolders, currentFilesAndFolders, "", ""]
 }
