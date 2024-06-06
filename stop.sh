45d4eba371a3a30076622f8318f1636b56bcf747
Pfannkuchen <hello@gougoule.ch>
2024-06-06 15:29:47 +0200

    add update.sh

--- stop.sh
+++ stop.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
 
-docker ps -q --filter ancestor=hub-for-things-frontend | xargs docker stop
-docker ps -q --filter ancestor=hub-for-things-backend | xargs docker stop
-
 docker ps -q --filter ancestor=gougoule-finance-frontend | xargs docker stop
 docker ps -q --filter ancestor=gougoule-finance-backend | xargs docker stop
---
+++ update.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# stop
+docker ps -q --filter ancestor=gougoule-finance-frontend | xargs docker stop
+docker ps -q --filter ancestor=gougoule-finance-backend | xargs docker stop
+
+# update
+cd backend
+docker build --pull -t gougoule-finance-backend .
+
+cd ../frontend
+docker build --pull -t gougoule-finance-frontend .
+
+# go back
+cd ../
+
+# start
+
+docker run --network="host" -d gougoule-finance-backend
+docker run -d -p 8765:3000 gougoule-finance-frontend
